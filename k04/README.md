50981506d916df67a594aafa29f0e4b9455f5404
PARK BAEGEUN <qorms115@gmail.com>
2020-06-28 13:41:12 +0900

    kadai4 complete

--- k04/README.md
+++ k04/README.md
@@ -1,6 +1,6 @@
 # 課題4 レポート
 
-aa83988848 薗田光太郎
+bb35317057 PARK BAEGEUN
 
 ## 課題
 
@@ -11,31 +11,35 @@ aa83988848 薗田光太郎
 この二つのファイルを読み取り，学籍番号，身長，性別の3つをメンバーとする構造体の配列で管理するとよい．
 
 ## ソースコードの説明
-
+ まず、腎臓の標本になってくれるファイル（ここではheights.csv）を呼んでいます。エラーが出たら終了
+ その次の標本になってくれる学生番号ファイル（IDs.csv）を呼んでいます。エラーが出たら終了
+ この二つのファイルを使用して、各学生番号に対応する伸長を付与して、データを保存する。
+ 最後に、学生番号に該当する情報があれば、そのファイルをロードします。存在しない学生番号なら終了。
 
 ## 入出力結果
-
-例えば，ID 45313125のデータを調べたいとき，
-
-```
-Input the filename of sample height : ../sample/heights.csv
-Input the filename of sample ID : ../sample/IDs.csv
-Which ID's data do you want? : 45313125
----
+データが存在する場合
+input the filename of sample height : heights.csv
+the filename of sample height : heights.csv
+input the filename of sample ID : IDs.csv
+the filename of sample ID : IDs.csv
+Which data do you want? : 45313125
+================================
 ID : 45313125
-gender : Female
-height : 152.4
-```
+gender : female
+height : 152.40
+============================
 
-例えば，ID 45313124のデータを調べたいとき，
 
-```
-Input the filename of sample height : ../sample/heights.csv
-Input the filename of sample ID : ../sample/IDs.csv
-Which ID's data do you want? : 45313124
----
+データが存在しない場合
+the filename of sample height : heights.csv
+input the filename of sample ID : IDs.csv
+the filename of sample ID : IDs.csv
+Which data do you want? : 45313124
+================================
 No data
-```
+============================
+
+
 
 ## 修正履歴
 
--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct list
+{
+    int gender;
+    double heights;
+    int ID;
+} list;
+
+int main(void)
+{
+    char fname[FILENAME_MAX];
+    char buf[256];
+    char buf2[256];
+    FILE *fp;
+    FILE *fp2;
+    int ID;
+    int flag = 0;
+    int n = 1;
+    int i = 0;
+    list Data[256];
+
+    printf("input the filename of sample height : ");
+    fgets(fname, sizeof(fname), stdin);
+    fname[strlen(fname) - 1] = '\0';
+    printf("the filename of sample height : %s\n", fname);
+    fp = fopen(fname, "r");
+    if (fp == NULL)
+    {
+        fputs("File open error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        sscanf(buf, "%d,%lf", &Data[i].gender, &Data[i].heights);
+        i++;
+    }
+
+    if (fclose(fp) == EOF)
+    {
+        fputs("file close error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("input the filename of sample ID : ");
+    fgets(fname, sizeof(fname), stdin);
+    fname[strlen(fname) - 1] = '\0';
+    printf("the filename of sample ID : %s\n", fname);
+    fp2 = fopen(fname, "r");
+    if (fp2 == NULL)
+    {
+        fputs("File open error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Which data do you want? : ");
+    scanf("%d", &ID);
+
+    while (fgets(buf2, sizeof(buf2), fp2) != NULL)
+    {
+        sscanf(buf2, "%d", &Data[n].ID);
+        if (Data[n].ID == ID)
+        {
+            flag = 1;
+            break;
+        }
+        else
+        {
+            n++;
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("================================\n");
+        printf("No data\n");
+    }
+    else
+    {
+        if (Data[n].gender == 1)
+        {
+            printf("================================\n");
+            printf("ID : %d\n", ID);
+            printf("gender : male\n");
+            printf("height : %5.2lf\n", Data[n].heights);
+        }
+        else
+        {
+            printf("================================\n");
+            printf("ID : %d\n", ID);
+            printf("gender : female\n");
+            printf("height : %5.2lf\n", Data[n].heights);
+        }
+    }
+    if (fclose(fp2) == EOF)
+    {
+        fputs("file close error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("============================\n");
+
+    return 0;
+}
 
